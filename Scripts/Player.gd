4f33ddfff4a12577d9dbcea90fdd613c8f34a1f7
DalosanESP <dalosanesp@gmail.com>
2022-06-08 00:11:31 +0200

    Animaciones de ataque y salto actualizados

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,11 +3,25 @@ extends KinematicBody2D
 const SPEED = 100
 const FLOOR = Vector2(0,-1)
 const GRAVITY = 12
-const JUMP_HEIGHT = 400
-onready var motion = Vector2.ZERO
+const JUMP_HEIGHT = 500
+const CAST_ENEMY = 22
+const CAST_WALL = 10
+onready var motion : Vector2 = Vector2.ZERO
+var can_move : bool
+
+
+var playback: AnimationNodeStateMachinePlayback #enlace con el animation tree
+
+func _ready():
+	playback = $AnimationTree.get("parameters/playback")
+	playback.start("Estatico")#estado inicial del arbol
+	$AnimationTree.active = true #para activar el arbol 
 
 func _process(delta):
 	motion_ctrl()
+	direction_ctrl()
+	salto_ctrl()
+	ataque_ctrl()
 	 
 func get_axis() -> Vector2:
 	var axisX = Vector2.ZERO
@@ -20,26 +34,73 @@ func get_axis() -> Vector2:
 func motion_ctrl():
 	motion.y += GRAVITY
 	
-	if get_axis().x == 1:
-		$AnimatedSprite.flip_h = false
-	elif get_axis().x == -1:
-		$AnimatedSprite.flip_h = true
-	
-	if get_axis().x != 0:
+	if can_move:
 		motion.x = get_axis().x * SPEED
-	else:
-		motion.x = 0
 		
-	if is_on_floor():
-		if get_axis().x != 0:
-			$AnimatedSprite.play("Caminar")
+		if get_axis().x == 0:
+			playback.travel("Estatico")
 		else:
-			$AnimatedSprite.play("Static")
-	
-	if Input.is_action_just_pressed("ui_accept"):
-		motion.y -= JUMP_HEIGHT
-	else:
-		if motion.y < 0:
-			$AnimatedSprite.play("Salto")
+			playback.travel("Caminar")
+		
+		if get_axis().x ==1:
+				$Sprite.flip_h = false
+		elif get_axis().x == -1:
+				$Sprite.flip_h = true
 			
 	motion = move_and_slide(motion,FLOOR)
+	
+func direction_ctrl():
+	match $Sprite.flip_h:
+		true:
+			$RayWall.cast_to.x = -CAST_WALL
+			$RayEnemy.cast_to.x = -CAST_ENEMY
+		false:
+			$RayWall.cast_to.x = CAST_WALL
+			$RayEnemy.cast_to.x = CAST_ENEMY
+func salto_ctrl():
+	match is_on_floor():
+		true:
+			can_move = true
+			$RayWall.enabled = false
+		
+			if Input.is_action_just_pressed("Salto"):
+				motion.y -= JUMP_HEIGHT
+		
+		false:
+			
+			$RayWall.enabled = true
+			
+			if motion.y < 0:
+				playback.travel("Salto")
+			
+			else:
+				playback.travel("Salto")
+			
+			if $RayWall.is_colliding():
+				can_move = false
+				
+				var col = $RayWall.get_collider()
+				
+func ataque_ctrl():
+	if is_on_floor():
+		if get_axis().x == 0 and Input.is_action_just_pressed("Ataque"):
+			match playback.get_current_node():
+				"Estatico":
+					playback.travel("Ataque1")
+				
+				"Ataque1":
+					playback.travel("Ataque2")
+					
+				"Ataque2":
+					playback.travel("Ataque3")
+				
+		if get_axis().x == 0 and Input.is_action_just_pressed("Combo"):
+			match playback.get_current_node():
+				"Estatico":
+					playback.travel("Combo1")
+				
+				"Combo1":
+					playback.travel("Combo2")
+					
+				"Combo2":
+					playback.travel("Combo3")
